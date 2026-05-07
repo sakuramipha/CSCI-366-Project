24c6e0ba7c4f6dac439d77354f0cec3b8acd84f6
August Preuss <augustp2546@gmail.com>
2026-05-06 19:39:35 -0500

    DBManager and added the .jar file so our SQL code will work

---
+++ MovieRentalProject/MovieRental/src/movierental/DBManager.java
@@ -0,0 +1,46 @@
+
+package movierental;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBManager {
+    static String jdbcURL = "jdbc:postgresql://localhost:5432/";
+    static String username = "";
+    static String password = "";
+    
+    private static Connection connection; 
+    
+    public DBManager(){
+        
+    }
+    
+    public static Connection getConnection() {
+        if (connection == null){
+            setConnection();
+        }
+        return connection;
+    }
+    
+    private static void setConnection(){
+        try {
+        Class.forName("org.postgresql.Driver");
+            
+        connection = DriverManager.getConnection(jdbcURL, username, password);
+        }
+        catch (ClassNotFoundException e){
+            System.out.println("Cannot load the postgresql driver.");
+        }
+        catch (SQLException e) {
+            System.out.println("Got a sql exception.");
+            e.printStackTrace();
+        }
+       }
+    
+    
+    
+    
+    }
+    
